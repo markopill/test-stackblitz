39209e1848431f043f1630bb64ae9d59287b3035
Marko Pill <marko.pill@boku.com>
2024-02-16 11:38:31 +0200

    add angular tutorial app

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
-import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import 'zone.js';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
-})
-export class App {
-  name = 'Angular';
-}
-
-bootstrapApplication(App);
+bootstrapApplication(AppComponent,
+    {
+      providers: [
+        provideProtractorTestingSupport(),
+        provideRouter(routeConfig)
+      ]
+    }
+).catch(err => console.error(err));
